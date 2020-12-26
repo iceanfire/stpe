4779c043fbde4c135491fe287c521e1581db8c24
Hadi Laasi <hadilaasi@fb.com>
2020-12-26 23:45:32 +0100

    added hack to have multi-line placeholder text, so people have an idea of what the expected format should look like (https://stackoverflow.com/questions/7189223/can-you-have-multiline-html5-placeholder-text-in-a-textarea)

--- src/wordcount.js
+++ src/wordcount.js
@@ -8,11 +8,37 @@ class WordCount extends React.Component {
         value : null
       }
       this.handleKeypress = this.handleKeypress.bind(this);
+      this._onFocus = this._onFocus.bind(this);
+      this._onBlur = this._onBlur.bind(this);
+      this.setValue = this.setValue.bind(this);
+      this.placeholder = "1. Call family \n2. Finish Stripe Assignment \n"
+      this.textInput = React.createRef();
     }
     
     handleKeypress(event) {
-      console.log('*****', event.target.value);
+      console.log(event.target.value)
       this.setState({ value:event.target.value });
+    }
+
+    componentDidMount() {
+      this.setValue(this.placeholder)
+    }
+
+    setValue(newValue){
+      this.textInput.current.value = newValue
+      this.state.value = newValue
+    }
+
+    _onFocus() {
+      if(this.state.value == this.placeholder){
+        this.setValue("1. ")
+      }
+    }
+
+    _onBlur(){
+      if(this.state.value == ""){
+        this.setValue(this.placeholder)
+      }
     }
     
     render(){
@@ -20,7 +46,14 @@ class WordCount extends React.Component {
           length = this.state.value?this.state.value.length:0;
       return(
         <div>
-          <textarea onChange={(event)=>this.handleKeypress(event)}>{this.state.value}</textarea>
+          <textarea id="" 
+            onChange={(event)=>this.handleKeypress(event)} 
+            onFocus={this._onFocus} 
+            onBlur={this._onBlur}
+            ref={this.textInput}
+          >
+            {this.state.value}
+          </textarea>
           <div>Count: {length}</div>
           <div>this.state.value: {this.state.value}</div>
         </div>
