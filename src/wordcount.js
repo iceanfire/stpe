801a859bf042fcbb11410661895c1603dc8a9216
Hadi Laasi <hadilaasi@fb.com>
2020-12-26 23:54:01 +0100

    simpler logic for placeholder, should work in most browsers + added benefit is better formatting of placeholder text. kept the on-blur/on-focus events, using them for managing the initial text instead

--- src/wordcount.js
+++ src/wordcount.js
@@ -11,17 +11,17 @@ class WordCount extends React.Component {
       this._onFocus = this._onFocus.bind(this);
       this._onBlur = this._onBlur.bind(this);
       this.setValue = this.setValue.bind(this);
-      this.placeholder = "1. Call family \n2. Finish Stripe Assignment \n"
+      this.placeholder = "1. Call family \n2. Finish Stripe Assignment \n";
+      this.startValue = "1. ";
       this.textInput = React.createRef();
     }
     
     handleKeypress(event) {
-      console.log(event.target.value)
       this.setState({ value:event.target.value });
     }
 
     componentDidMount() {
-      this.setValue(this.placeholder)
+      this.textInput.current.placeholder = this.placeholder
     }
 
     setValue(newValue){
@@ -30,17 +30,17 @@ class WordCount extends React.Component {
     }
 
     _onFocus() {
-      if(this.state.value == this.placeholder){
-        this.setValue("1. ")
+      if(!this.state.value){
+        this.setValue(this.startValue);
       }
     }
 
-    _onBlur(){
-      if(this.state.value == ""){
-        this.setValue(this.placeholder)
+    _onBlur() {
+      if(this.state.value == this.startValue){
+        this.setValue("")
       }
     }
-    
+
     render(){
       let count = 0,
           length = this.state.value?this.state.value.length:0;
