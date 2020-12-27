b500a7c31e3cb639e7a8318330b2a0f07dbea74a
Hadi Laasi <hadilaasi@fb.com>
2020-12-27 22:13:03 +0100

    install react router via npm + clean up structure of app.js to make it easier to use reactrouter

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import React from 'react';
 import WordCount from './wordcount.js'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
@@ -13,10 +20,12 @@ function App() {
           <p>Got too much going on and don't know where to start?</p>
           <p>Just type in your to-do list below and we'll get some of the smarest PMs around to prioritise and build a roadmap for your to-do list!</p>
         </div>
+      </header>
+      <body>
         <form>
           <WordCount></WordCount>
         </form>
-      </header>
+      </body>
     </div>
   );
 }
